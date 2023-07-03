84bc7333db9f396cfb7a7b325e65e146d29b526f
Michael Sesuraj <msesuraj66@gmail.com>
2023-07-03 19:32:25 +1000

    .net 6

---
+++ consoleapp/Menu.cs
@@ -0,0 +1,78 @@
+﻿using System;
+ 
+namespace ConsoleApp1
+{
+    class Menu
+    {
+        static void Main(string[] args)
+        {
+            bool showMenu = true;
+            while (showMenu)
+            {
+                showMenu = MainMenu();
+            }
+        }
+        private static bool MainMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("Effective Fruits and Vegetables");
+            Console.WriteLine("1) Opening Hours");
+            Console.WriteLine("2) Calendar");
+            Console.WriteLine("3) Shift Times");
+            Console.WriteLine("4) Employees");
+            Console.WriteLine("9) Exit");
+            Console.Write("\r\nSelect an option: ");
+ 
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    OpenHours cl = new OpenHours();
+                    cl.TestMethod(); 
+                    return true;
+                case "2":
+                    RemoveWhitespace();
+                    return true;
+                case "3":
+
+                    return true;
+                case "4":
+                    return true;
+                case "9":
+                    return false;
+                default:
+                    return true;
+            }
+        }
+ 
+        private static string CaptureInput()
+        {
+            Console.Write("Enter the string you want to modify: ");
+            return Console.ReadLine();
+        }
+ 
+        private static void ReverseString()
+        {
+            Console.Clear();
+            Console.WriteLine("Reverse String");
+ 
+            char[] charArray = CaptureInput().ToCharArray();
+            Array.Reverse(charArray);
+            DisplayResult(String.Concat(charArray));
+        }
+ 
+        private static void RemoveWhitespace()
+        {
+            Console.Clear();
+            Console.WriteLine("Remove Whitespace");
+ 
+            DisplayResult(CaptureInput().Replace(" ", ""));
+        }
+ 
+        private static void DisplayResult(string message)
+        {
+            Console.WriteLine($"\r\nYour modified string is: {message}");
+            Console.Write("\r\nPress Enter to return to Main Menu");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ consoleapp/OpenHours.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class OpenHours
+    {
+          public void TestMethod()
+        {
+            Console.WriteLine("Inside OpenHours.TestMethod");
+            Console.ReadLine();
+         }
+    }
+}
+/* location, day start time, end time  
+ File.Create("fileName");
+using System.IO;
+             string path = @"D:\Development\OwainCodes_Blogs\testFile.txt";
+            var output = new OutputFile(path, text);
+https://www.owain.codes/blogs/2021/may/create-a-console-app-that-saves-to-a-file/
+(write into file)
+*/
