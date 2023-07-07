e910f8235a77474d3c2706ef2de78eefd22f6442
Michael Sesuraj <msesuraj66@gmail.com>
2023-07-07 21:12:20 +1000

    7thJuly

--- consoleapp/OpenHours.cs
+++ consoleapp/OpenHours.cs
@@ -8,14 +8,23 @@ using System.IO;
 
 namespace ConsoleApp
 {
+    public class OpenHoursRecord
+    {
+        public string? LocationId{ get; set; } 
+        public string? Day{ get; set; }
+        public string? StartTime{ get; set; }
+        public string? EndTime{ get; set; }
+    }
     public class OpenHours
     {
          StreamReader csvData = null;//creates a variable to load the data from the csv file
+         List<OpenHoursRecord> openHoursRecords = new List<OpenHoursRecord>();
+
          public void OpenHoursMain()
             {
                 CsvLoad(); // loads file to csv file
                 CsvToList(); // reads file to lists
-                Console.WriteLine("Inside OpenHours.TestMethod");
+                Console.WriteLine("Inside OpenHours.OpenHoursMain");
                 Console.ReadLine();
 
            }
@@ -42,12 +51,19 @@ namespace ConsoleApp
             void CsvToList ()
             {           
                 List<string> LineValues = new List<string>();
-                csvData.ReadLine(); // skip first
+                //csvData.ReadLine(); // skip first
                 string line;
                 while ((line = csvData.ReadLine()) != null)
                     {
                         Console.WriteLine(line);
                         LineValues = line.Split(',').ToList();
+                    
+                        openHoursRecords.Add(new OpenHoursRecord { LocationId = LineValues[0].ToString(),
+                                                              Day = LineValues[1].ToString(),
+                                                              StartTime = LineValues[2].ToString(),
+                                                              EndTime =  LineValues[3].ToString() 
+                                                              });
+
                         /*
                         gender.Add(LineValues[5]);
                         sales.Add(Int32.Parse(LineValues[4]));
@@ -55,6 +71,21 @@ namespace ConsoleApp
                         lastnames.Add(LineValues[2]);
                         */
                     }
+                LineValues.ForEach(Console.WriteLine);
+                Console.WriteLine("Opening Hours in total: " + openHoursRecords.Count);
+
+                foreach (OpenHoursRecord openHoursRecord in openHoursRecords)
+                    {
+                        Console.WriteLine(openHoursRecord.Day);
+                    }
+                /*
+                // Print only those who are above the limit
+                foreach (Person person in persons) {
+                if (person.age >= ageLimit)
+                { 
+                    Console.WriteLine(person);
+                }
+                */
 
             /// <summary>
             /// writes the csv into a list for each coloumn
@@ -62,6 +93,21 @@ namespace ConsoleApp
             }
 
     }
+/* write to file 
+https://stackoverflow.com/questions/13815634/how-to-create-a-csv-file-from-liststring
+File.WriteAllLines("text.txt", lst.Select(x => string.Join(",", x)));
+https://www.makeuseof.com/csv-file-c-sharp-save-data/
+
+https://www.youtube.com/watch?v=fRaSeLYYrcQ
+
+https://www.clintmcmahon.com/how-to-open-a-csv-file-in-the-browser/
+
+https://gist.github.com/luisdeol/c2c276796a92c8e3246ce2cd3e17e1df
+
+https://www.c-sharpcorner.com/article/how-to-read-data-from-csv-file-in-c-sharp/
+https://www.c-sharpcorner.com/article/c-sharp-list/
+*/
+
 
 }
 
