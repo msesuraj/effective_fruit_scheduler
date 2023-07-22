59f296cf27f15bcc76a6ee86c04ceb56c05aee0c
Michael Sesuraj <msesuraj66@gmail.com>
2023-07-22 22:26:54 +1000

    22Jul-X

--- WinFormsApp1/Employees.Designer.cs
+++ WinFormsApp1/Employees.Designer.cs
@@ -28,12 +28,44 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Employee";
+            buttonMainMenuE = new Button();
+            label1 = new Label();
+            SuspendLayout();
+            // 
+            // buttonMainMenuE
+            // 
+            buttonMainMenuE.Location = new Point(920, 12);
+            buttonMainMenuE.Name = "buttonMainMenuE";
+            buttonMainMenuE.Size = new Size(112, 34);
+            buttonMainMenuE.TabIndex = 0;
+            buttonMainMenuE.Text = "MainMenu";
+            buttonMainMenuE.UseVisualStyleBackColor = true;
+            buttonMainMenuE.Click += buttonMainMenuE_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(-1, 48);
+            label1.Name = "label1";
+            label1.Size = new Size(0, 25);
+            label1.TabIndex = 1;
+            // 
+            // Employees
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1062, 450);
+            Controls.Add(label1);
+            Controls.Add(buttonMainMenuE);
+            Name = "Employees";
+            Text = "FV&F Employee";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button buttonMainMenuE;
+        private Label label1;
     }
 }
--- WinFormsApp1/Employees.cs
+++ WinFormsApp1/Employees.cs
@@ -16,5 +16,12 @@ namespace WinFormsApp1
         {
             InitializeComponent();
         }
+
+        private void buttonMainMenuE_Click(object sender, EventArgs e)
+        {
+            MainMenu mainMenu = new MainMenu();
+            mainMenu.Show();
+            this.Hide();
+        }
     }
 }
