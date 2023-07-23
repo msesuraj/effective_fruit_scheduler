e9b6c3b9da6e8444753d32d912bcdcaf84012e17
Michael Sesuraj <msesuraj66@gmail.com>
2023-07-23 22:55:28 +1000

    23July=I

--- WinFormsApp1/Employees.Designer.cs
+++ WinFormsApp1/Employees.Designer.cs
@@ -30,6 +30,10 @@
         {
             buttonMainMenuE = new Button();
             label1 = new Label();
+            labelEmployeeNumber = new Label();
+            textEmployeeNumber = new TextBox();
+            labelEmployeeName = new Label();
+            textEmployeeName = new TextBox();
             SuspendLayout();
             // 
             // buttonMainMenuE
@@ -44,17 +48,54 @@
             // 
             // label1
             // 
-            label1.AutoSize = true;
-            label1.Location = new Point(-1, 48);
+            label1.Anchor = AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            label1.BorderStyle = BorderStyle.Fixed3D;
+            label1.Location = new Point(1, 69);
             label1.Name = "label1";
-            label1.Size = new Size(0, 25);
-            label1.TabIndex = 1;
+            label1.Size = new Size(1063, 10);
+            label1.TabIndex = 4;
+            // 
+            // labelEmployeeNumber
+            // 
+            labelEmployeeNumber.AutoSize = true;
+            labelEmployeeNumber.Location = new Point(36, 96);
+            labelEmployeeNumber.Name = "labelEmployeeNumber";
+            labelEmployeeNumber.Size = new Size(160, 25);
+            labelEmployeeNumber.TabIndex = 5;
+            labelEmployeeNumber.Text = "Employee Number";
+            // 
+            // textEmployeeNumber
+            // 
+            textEmployeeNumber.Location = new Point(247, 96);
+            textEmployeeNumber.Name = "textEmployeeNumber";
+            textEmployeeNumber.Size = new Size(150, 31);
+            textEmployeeNumber.TabIndex = 6;
+            // 
+            // labelEmployeeName
+            // 
+            labelEmployeeName.AutoSize = true;
+            labelEmployeeName.Location = new Point(479, 102);
+            labelEmployeeName.Name = "labelEmployeeName";
+            labelEmployeeName.Size = new Size(142, 25);
+            labelEmployeeName.TabIndex = 7;
+            labelEmployeeName.Text = "Employee Name";
+            // 
+            // textEmployeeName
+            // 
+            textEmployeeName.Location = new Point(647, 102);
+            textEmployeeName.Name = "textEmployeeName";
+            textEmployeeName.Size = new Size(366, 31);
+            textEmployeeName.TabIndex = 8;
             // 
             // Employees
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1062, 450);
+            Controls.Add(textEmployeeName);
+            Controls.Add(labelEmployeeName);
+            Controls.Add(textEmployeeNumber);
+            Controls.Add(labelEmployeeNumber);
             Controls.Add(label1);
             Controls.Add(buttonMainMenuE);
             Name = "Employees";
@@ -67,5 +108,9 @@
 
         private Button buttonMainMenuE;
         private Label label1;
+        private Label labelEmployeeNumber;
+        private TextBox textEmployeeNumber;
+        private Label labelEmployeeName;
+        private TextBox textEmployeeName;
     }
 }
